46671b0e97740271d91db44fdd932c7a1dff5871
Microchip Technology <http://support.microchip.com>
2020-05-27 15:07:30 +0530

    Disabled PB for the ports

--- PSF/Demo/PSF_EVB_Source_Pro/PowerDelivery/PSFSourcePro_BootCfg.h
+++ PSF/Demo/PSF_EVB_Source_Pro/PowerDelivery/PSFSourcePro_BootCfg.h
@@ -156,11 +156,11 @@ HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
 
 /* Note: By default, PB is disabled for the system since support for 
    PPS + PB coexistence is planned for future phases. If PB needs to be 
-   enabled, change the value of following macro to 0x10U. This will
-   enable PB for the system with FCFS as Selection Algorithm. */
+   enabled, change the value of CFG_PB_ENABLE macro to 0x10U and 
+   CFG_PB_PORT_ENABLE to 0x01U. This will enable PB for the system 
+   with FCFS as Selection Algorithm and for the ports. */
 #define CFG_PB_ENABLE                         0x00U 
-/* Default Enable PB for port 0 with Priority as 0*/
-#define CFG_PB_PORT_ENABLE                    0x01U
+#define CFG_PB_PORT_ENABLE                    0x00U
 
 #define CFG_PDO_VOLTAGE_POS                   10 
 #define CFG_PDO_VOLTAGE_UNIT                  50
