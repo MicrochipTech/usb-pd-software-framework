1e68405090bd8bcf15cc415b0274b91d277995df
Microchip Technology <http://support.microchip.com>
2020-07-10 12:54:27 -0700

    source lite demo added for basic source app example board

---
+++ PSF/Demo/UPD301B_Basic_Source_AE_Source_Lite/PowerDelivery/PSFSourceLite_App.h
@@ -0,0 +1,84 @@
+/***************************************************************************
+Power Delivery Source Pro Application header file
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    PSFSourceLite_App.h
+
+  Description:
+    This header file contains user application specific functions and interfaces
+************************************************************************** */
+/*******************************************************************************
+Copyright ©  [2019-2020] Microchip Technology Inc. and its subsidiaries.
+
+Subject to your compliance with these terms, you may use Microchip software and
+any derivatives exclusively with Microchip products. It is your responsibility
+to comply with third party license terms applicable to your use of third party
+software (including open source software) that may accompany Microchip software.
+
+THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER EXPRESS,
+IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES
+OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A PARTICULAR PURPOSE. IN
+NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS BEEN
+ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST
+EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY
+RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY, THAT YOU
+HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+
+#ifndef _PSFSOURCELITE_APP_H
+#define _PSFSOURCELITE_APP_H
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Section: Included Files                                                    */
+/* ************************************************************************** */
+/* ************************************************************************** */
+#include <PSF_APIHook.h>
+#include "psf_stdinc.h"
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Section: Defines and constants                                                   */
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Defines major and minor version of PSF Source Lite Application*/
+
+#define APP_REV_MAJOR	0x01
+#define APP_REV_MINOR	0x04
+
+#define APP_VOLTAGE_0mV         0
+#define APP_VOLTAGE_5000mV      5000
+#define APP_VOLTAGE_9000mV      9000
+#define APP_VOLTAGE_15000mV     15000
+#define APP_VOLTAGE_20000mV     20000
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Interface Functions
+// *****************************************************************************
+// *****************************************************************************
+void App_SetMCUIdle(); 
+
+UINT8 App_HandlePSFEvents(UINT8 u8PortNum, eMCHP_PSF_NOTIFICATION u8PDEvent);
+
+void App_GPIOControl_Init(UINT8 u8PortNum, eMCHP_PSF_GPIO_FUNCTIONALITY eGPIOFunc);
+
+void App_GPIOControl_Drive(UINT8 u8PortNum, eMCHP_PSF_GPIO_FUNCTIONALITY eGPIOFunc,
+                                    eMCHP_PSF_GPIO_DRIVE_VAL eGPIODrive);
+
+UINT8 App_PortPowerInit(UINT8 u8PortNum);
+
+void App_PortPowerSetPower(UINT8 u8PortNum, UINT16 u16Voltage, UINT16 u16Current);
+
+
+#endif /* _PSFSOURCELITE_APP_H */
+
+/* *****************************************************************************
+ End of File
+ */
---
+++ PSF/Demo/UPD301B_Basic_Source_AE_Source_Lite/firmware/UPD301B_Basic_Source_AE_Source_Lite.X/build/default/production/_ext/318156199/PSFSourceLite_App.o.d
@@ -0,0 +1,55 @@
+build/default/production/_ext/318156199/PSFSourceLite_App.o:  \
+ ../../PowerDelivery/PSFSourceLite_App.c  \
+ ../../../../Source/include/psf_stdinc.h  \
+ ../../../../Source/include/generic_defs.h  \
+ ../../PowerDelivery/PSF_Config.h  \
+../../PowerDelivery/PSFSourceLite_App.h  \
+ ../../../../SOC_Portable/SAMD20/PSF_APIHook.h  \
+ ../../../../SOC_Portable/SAMD20/Drivers.h  \
+ ../src/../../firmware/src/config/default/peripheral/tc/plib_tc0.h  \
+ ../src/packs/ATSAMD20E16_DFP/device.h  \
+ ../src/packs/ATSAMD20E16_DFP/atsamd20e16.h  \
+ ../src/packs/CMSIS/CMSIS/Core/Include/core_cm0plus.h  \
+ ../src/packs/CMSIS/CMSIS/Core/Include/cmsis_version.h  \
+ ../src/packs/CMSIS/CMSIS/Core/Include/cmsis_compiler.h  \
+ ../src/packs/CMSIS/CMSIS/Core/Include/cmsis_gcc.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/ac.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/adc.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/dac.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/dsu.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/eic.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/evsys.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/gclk.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/nvmctrl.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/pac.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/pm.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/port.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/ptc.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/rtc.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/sercom.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/sysctrl.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/tc.h  \
+ ../src/packs/ATSAMD20E16_DFP/component/wdt.h  \
+ ../src/packs/ATSAMD20E16_DFP/pio/samd20e16.h  \
+ ../src/config/default/device_cache.h  \
+ ../src/config/default/toolchain_specifics.h  \
+ ../src/../../firmware/src/config/default/peripheral/tc/plib_tc_common.h  \
+ ../src/../../firmware/src/config/default/peripheral/sercom/spim/plib_sercom0_spi.h  \
+ ../src/../../firmware/src/config/default/peripheral/sercom/spim/plib_sercom_spi_common.h  \
+ ../src/../../firmware/src/config/default/peripheral/port/plib_port.h  \
+ ../src/../../firmware/src/config/default/peripheral/eic/plib_eic.h  \
+ ../../PowerDelivery/PSFSourceLite_BootCfg.h  \
+ ../../../../Source/include/portpower_control.h  \
+ ../../../../Source/include/debug.h  \
+../../../../Source/include/pd_timer.h  \
+ ../../../../Source/include/typeC_control.h  \
+ ../../../../Source/include/policy_manager.h  \
+ ../../../../Source/include/policy_engine.h  \
+ ../../../../Source/include/upd_interrupts.h  \
+ ../../../../Source/include/upd_hw.h  \
+ ../../../../Source/include/protocol_layer.h  \
+ ../../../../Source/include/int_globals.h  \
+ ../../../../Source/include/policy_engine_fwup.h  \
+ ../../../../Source/include/ProjectVersion.h  \
+ ../../../../Source/include/timer_defs.h  \
+ ../../../../Source/UPD_PIO/upd350_pio_drivers.h 
