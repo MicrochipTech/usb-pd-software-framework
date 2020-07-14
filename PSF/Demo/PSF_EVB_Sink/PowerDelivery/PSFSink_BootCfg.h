6bb057f895008e31c25204a4eb083c6943839aa5
Ishwarya Ashok Kumar <ishwarya.ashokkumar@microchip.com>
2020-07-14 14:53:55 +0530

    Added RPVAL_POS and ENDIS_POS config macros in src ans snk demos

--- PSF/Demo/PSF_EVB_Sink/PowerDelivery/PSFSink_BootCfg.h
+++ PSF/Demo/PSF_EVB_Sink/PowerDelivery/PSFSink_BootCfg.h
@@ -45,8 +45,10 @@ HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
 #define SILICON_VERSION         0x00U
 
 #define CFG_PORT_POWER_ROLE                0U //Sink
-#define CFG_PORT_RP_CURRENT_VALUE          (0U << TYPEC_PORT_RPVAL_POS) /*For Sink Rp value is 0*/
-#define CFG_PORT_ENABLE                    (1U << TYPEC_PORT_ENDIS_POS)
+#define CFG_PORT_RPVAL_POS                 3U
+#define CFG_PORT_ENDIS_POS                 5U
+#define CFG_PORT_RP_CURRENT_VALUE          (0U << CFG_PORT_RPVAL_POS) /*For Sink Rp value is 0*/
+#define CFG_PORT_ENABLE                    (1U << CFG_PORT_ENDIS_POS)
 #define CFG_PORT_SOURCE_NUM_OF_PDOS        4U
 #define CFG_PORT_SOURCE_USB_SUSP           0U
 #define CFG_PORT_SOURCE_UNCONSTARINED_PWR  1U
