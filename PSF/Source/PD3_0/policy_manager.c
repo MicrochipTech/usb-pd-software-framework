6ef0c00a82e063b81651951d51793968dbd3dc23
Microchip Technology <http://support.microchip.com>
2020-07-15 12:07:55 +0530

    Fixed build errors in source demos

--- PSF/Source/PD3_0/policy_manager.c
+++ PSF/Source/PD3_0/policy_manager.c
@@ -657,34 +657,6 @@ void DPM_OnTypeCDetach(UINT8 u8PortNum)
     DPM_ClearAllClientRequests(u8PortNum);
 
 }
-void DPM_OnTypeCAttach(UINT8 u8PortNum)
-{
-    /*If the port is Source set Internal event to initiate Get_SinkCaps*/
-    if ((DPM_GET_CURRENT_POWER_ROLE(u8PortNum) == PD_ROLE_SOURCE) && (TRUE != DPM_IS_PB_ENABLED(u8PortNum)))
-    {
-        DPM_RegisterInternalEvent(u8PortNum, DPM_INT_EVT_INITIATE_GET_SINK_CAPS);
-    }
-    
-    /*Evaluate swap and register internal event*/
-    if (DPM_REQUEST_SWAP == DPM_EvaluateRoleSwap (u8PortNum, eVCONN_SWAP_INITATE))
-    {
-        DPM_RegisterInternalEvent(u8PortNum, DPM_INT_EVT_INITIATE_VCONN_SWAP);
-    }
-#if (TRUE == INCLUDE_PD_DR_SWAP)
-    if (DPM_REQUEST_SWAP == DPM_EvaluateRoleSwap (u8PortNum, eDR_SWAP_INITIATE))
-    {
-        DPM_RegisterInternalEvent(u8PortNum, DPM_INT_EVT_INITIATE_DR_SWAP);
-    }
-#endif /*INCLUDE_PD_DR_SWAP*/
-    
-#if (TRUE == INCLUDE_PD_PR_SWAP)
-    if (DPM_REQUEST_SWAP == DPM_EvaluateRoleSwap (u8PortNum, ePR_SWAP_INITIATE))
-    {
-        DPM_RegisterInternalEvent(u8PortNum, DPM_INT_EVT_INITIATE_PR_SWAP);
-    } 
-#endif /*INCLUDE_PD_PR_SWAP*/
-    
-}
 
 void DPM_UpdateDataRole (UINT8 u8PortNum, UINT8 u8DataRoleChange)
 {
@@ -918,6 +890,36 @@ UINT8 DPM_ReturnPowerStatus (UINT8 u8PortNum)
 
 #endif /*INCLUDE_PD_SOURCE_PPS*/ 
 #endif /*INCLUDE_PD_SOURCE*/  
+
+/*********************************DPM TypeC Attach API**************************************/
+void DPM_OnTypeCAttach(UINT8 u8PortNum)
+{
+    /*If the port is Source set Internal event to initiate Get_SinkCaps*/
+    if ((DPM_GET_CURRENT_POWER_ROLE(u8PortNum) == PD_ROLE_SOURCE) && (TRUE != DPM_IS_PB_ENABLED(u8PortNum)))
+    {
+        DPM_RegisterInternalEvent(u8PortNum, DPM_INT_EVT_INITIATE_GET_SINK_CAPS);
+    }
+    
+    /*Evaluate swap and register internal event*/
+    if (DPM_REQUEST_SWAP == DPM_EvaluateRoleSwap (u8PortNum, eVCONN_SWAP_INITATE))
+    {
+        DPM_RegisterInternalEvent(u8PortNum, DPM_INT_EVT_INITIATE_VCONN_SWAP);
+    }
+#if (TRUE == INCLUDE_PD_DR_SWAP)
+    if (DPM_REQUEST_SWAP == DPM_EvaluateRoleSwap (u8PortNum, eDR_SWAP_INITIATE))
+    {
+        DPM_RegisterInternalEvent(u8PortNum, DPM_INT_EVT_INITIATE_DR_SWAP);
+    }
+#endif /*INCLUDE_PD_DR_SWAP*/
+    
+#if (TRUE == INCLUDE_PD_PR_SWAP)
+    if (DPM_REQUEST_SWAP == DPM_EvaluateRoleSwap (u8PortNum, ePR_SWAP_INITIATE))
+    {
+        DPM_RegisterInternalEvent(u8PortNum, DPM_INT_EVT_INITIATE_PR_SWAP);
+    } 
+#endif /*INCLUDE_PD_PR_SWAP*/
+    
+}
 
 /*********************************DPM VDM Cable APIs**************************************/
 UINT8 DPM_StoreVDMECableData(UINT8 u8PortNum, UINT8 u8SOPType, UINT16 u16Header, UINT32* u32DataBuf)
